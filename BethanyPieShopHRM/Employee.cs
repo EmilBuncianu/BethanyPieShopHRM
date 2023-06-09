e52cf3bcbc001f6012c90cd8c38dbec0c62e50e2
emil.buncianu <erbuncianu@gmail.com>
2023-06-09 16:08:08 +0300

    Add project files.

---
+++ BethanyPieShopHRM/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanyPieShopHRM
+{
+    public class Employee
+    {
+        public string firstName = string.Empty;
+        public string lastName = string.Empty;
+        public string email = string.Empty;
+        public int numberOfhoursWorked;
+        public double wage;
+        public double hourlyRate=5;
+        const int minimalHoursWorkedHoursUnit = 1;
+        public DateTime birthDay;
+        public Employee(string firstName, string lastName, string email, DateTime birthDay) 
+        {
+        
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.birthDay = birthDay;
+
+        }
+
+
+        public void PerformWork()
+        {
+            //numberOfhoursWorked++;
+            PerformWork(minimalHoursWorkedHoursUnit);
+         //   Console.WriteLine($"{firstName} {lastName} has worked {numberOfhoursWorked}");
+        }
+        public void PerformWork(int numOfHours)
+        {
+            numberOfhoursWorked += numOfHours;
+            Console.WriteLine($"{firstName} {lastName} has worked {numOfHours}");
+        }
+        public double ReceiveWage(bool resetHours = true)
+        {
+            wage = numberOfhoursWorked * hourlyRate;
+            Console.WriteLine($"{firstName} {lastName} receive {wage} for {numberOfhoursWorked} hours worked");
+            if( resetHours ) { numberOfhoursWorked = 0; }
+
+            return wage;
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst name {firstName}\nLastName {lastName}\nEmail {email}\nBirthday {birthDay.ToShortDateString}\n");
+        }
+
+
+
+    }
+}
---
+++ BethanyPieShopHRM/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BethanyPieShopHRM;
+using static BethanyPieShopHRM.Employee;
+
+Console.WriteLine("Creating an employee");
+
+Employee betany = new Employee("Bethany","Smith","bethany@snowball.be", new DateTime (1979,1,16));
+
+
+betany.DisplayEmployeeDetails();
+betany.PerformWork();
+betany.PerformWork();
+betany.PerformWork();
+betany.PerformWork();
+betany.PerformWork();
+
+betany.PerformWork(5);
+double receiveWageBethany = betany.ReceiveWage(true);
+Console.WriteLine($"We paide to Bethany{ receiveWageBethany} Euros");
+Console.ReadKey();
